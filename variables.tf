ae13d7b5f3101c867c4851292e144067e5617436
Tomek <tomekdevops@gmail.com>
2026-04-13 19:49:22 +0200

    Add Terraform versions and variables configuration

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "europe-central2"
+}
+
+variable "cluster_name" {
+  description = "GKE Cluster name"
+  type        = string
+  default     = "tomek-devops-portfolio-cluster"
+}
+
+variable "node_count" {
+  description = "Number of nodes"
+  type        = number
+  default     = 2
+}
+
+variable "machine_type" {
+  description = "GCP machine type"
+  type        = string
+  default     = "e2-small"
+}
